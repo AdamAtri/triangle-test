ecc1e8c77c386028f2916424a5f4a09d73211187
Adam Atri <adam@atricoware.com>
2022-09-06 17:32:34 -0400

    adds detailed error message

--- app/main-view-model.ts
+++ app/main-view-model.ts
@@ -2,6 +2,15 @@ import { Observable } from '@nativescript/core'
 import gsap from 'gsap';
 import { TriangleType } from './view/triangle/triangle';
 
+export type ErrorString = 
+  'invalid or incorrect number of inputs' | 
+  'inputs must be numbers' |
+  'inputs must be greater than 0' | 
+  'a + b <= c' | 
+  'b + c <= a' | 
+  'c + a <= b' | 
+  'unknown error' |
+  null;
 
 
 export class TriangleModel extends Observable {
@@ -10,25 +19,32 @@ export class TriangleModel extends Observable {
     this.triangleType = this._determineTriangleType(values);
   }
 
-  public _validateSideValues(values:Array<number>):boolean {
-    // todo: create a function that validates the values using the Triangle Inequality Theorum
-    // (see tests)
+  public _validateSideValues(values:Array<number>):ErrorString {
     if (! values || values.length !== 3)
-      return false;
+      return 'invalid or incorrect number of inputs';
     const [a, b, c] = values;
     if (typeof a != 'number' || typeof b != 'number' || typeof c != 'number')
-      return false;
+      return 'inputs must be numbers';
     if (a <= 0 || b <= 0 || c <= 0)
-      return false;
+      return 'inputs must be greater than 0';
     if ((a + b > c) && (b + c > a) && (c + a > b))
-      return true;
-    return false;
+      return null;
+    else if (a + b <= c)
+      return 'a + b <= c';
+    else if (b + c <= a)
+      return 'b + c <= a';
+    else if (c + a <= b)
+      return 'c + a <= b';
+    return 'unknown error';
   }
 
   public _determineTriangleType(values:Array<number>):TriangleType {
-    // todo: create a function that returns the triangle type based on the values provided
-    if (! this._validateSideValues(values))
+    this.errorMessage = null;
+    const error = this._validateSideValues(values);
+    if (error) {
+      this.errorMessage = error;
       return null;
+    }
     const [a, b, c] = values;
     if (a == b && b == c) return 'equilateral';
     else if (a != b && b !== c && c != a) return 'scalene';
@@ -42,5 +58,13 @@ export class TriangleModel extends Observable {
     if (this._triangleType === v) return;
     this._triangleType = v;
     this.notifyPropertyChange('triangleType', v);
+  }
+
+  private _errorMessage:string;
+  public get errorMessage():string { return this._errorMessage; }
+  public set errorMessage(v:string) {
+    if (this._errorMessage === v) return;
+    this._errorMessage = v;
+    this.notifyPropertyChange('errorMessage', v);
   }
 }
--- app/view/triangle/triangle.ts
+++ app/view/triangle/triangle.ts
@@ -1,4 +1,4 @@
-import { GridLayout, Image, ImageSource, Label, Property } from "@nativescript/core";
+import { Color, CoreTypes, GridLayout, Image, ImageSource, Label, Property } from "@nativescript/core";
 
 export type TriangleType = 'equilateral'|'scalene'|'isosceles'|null;
 
@@ -6,11 +6,13 @@ export class Triangle extends GridLayout {
   constructor() {
     super();
     this.width = '100%' as any;
-    this.rows = 'auto, auto';
+    this.rows = 'auto, auto, auto';
     this.columns = '*';
     this.horizontalAlignment = 'center';
     this.addChild(this.image);
     this.addChild(this.label);
+    // todo: add errorLabel
+    this.addChild(this.errorLabel);
   }
 
   private _triangleType:TriangleType;
@@ -22,10 +24,14 @@ export class Triangle extends GridLayout {
       this.image.visibility = 'visible';
       this.image.src = `~/assets/${v}.png`;
       this.label.text = capitalize(v) + ' Triangle';
+      // @ts-ignore
+      this.label.color = 'black';
     }
     else {
       this.image.visibility = 'collapse';
       this.label.text = 'not a valid triangle';
+      //@ts-ignore
+      this.label.color = '#EE4444';
     }
     this.requestLayout();
   } 
@@ -51,6 +57,34 @@ export class Triangle extends GridLayout {
       this._label.horizontalAlignment = 'center';
     }
     return this._label;
+  }
+
+  // todo: add error message handling
+  private _error:string;
+  public get error():string { return this._error; }
+  public set error(v:string) {
+    if (this._error == v) return;
+    this._error = v;
+    if (v) {
+      this.errorLabel.text = v;
+      this.errorLabel.visibility = 'visible';
+    }
+    else {
+      this.errorLabel.visibility = 'collapse';
+    }
+  }
+
+  protected _errorLabel:Label;
+  public get errorLabel():Label {
+    if (! this._errorLabel) {
+      this._errorLabel = new Label();
+      this._errorLabel.style.fontFamily = 'monospace';
+      this._errorLabel.style.fontWeight = 'bold';
+      this._errorLabel.visibility = 'collapse';
+      this._errorLabel.row = 2;
+      this._errorLabel.horizontalAlignment = 'center';
+    }
+    return this._errorLabel;
   }
 }
 
