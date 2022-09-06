bd28b8599bdcfdac790307b8635c3812619fbefe
Adam Atri <adam@atricoware.com>
2022-09-06 15:31:31 -0400

    remove solutions

--- app/main-view-model.ts
+++ app/main-view-model.ts
@@ -1,5 +1,4 @@
 import { Observable } from '@nativescript/core'
-import gsap from 'gsap';
 import { TriangleType } from './view/triangle/triangle';
 
 
@@ -7,32 +6,20 @@ import { TriangleType } from './view/triangle/triangle';
 export class TriangleModel extends Observable {
 
   onTap = (values:Array<number>) => {
-    this.triangleType = this._determineTriangleType(values);
+    // todo: assign the triangle type based on the values supplied
+    alert('Supplied values: ' + values + '\nNow make it work');
   }
 
   public _validateSideValues(values:Array<number>):boolean {
     // todo: create a function that validates the values using the Triangle Inequality Theorum
     // (see tests)
-    if (! values || values.length !== 3)
-      return false;
-    const [a, b, c] = values;
-    if (typeof a != 'number' || typeof b != 'number' || typeof c != 'number')
-      return false;
-    if (a <= 0 || b <= 0 || c <= 0)
-      return false;
-    if ((a + b > c) && (b + c > a) && (c + a > b))
-      return true;
     return false;
   }
 
   public _determineTriangleType(values:Array<number>):TriangleType {
-    // todo: create a function that returns the triangle type based on the values provided
     if (! this._validateSideValues(values))
       return null;
-    const [a, b, c] = values;
-    if (a == b && b == c) return 'equilateral';
-    else if (a != b && b !== c && c != a) return 'scalene';
-    else if (a == b || b == c || c == a) return 'isosceles';
+    // todo: create a function that returns the triangle type based on the values provided
     else return null;
   }
 
